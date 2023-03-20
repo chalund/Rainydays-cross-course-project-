8fbfeac71b32ade646e5fd4977ff30ea8e61b00e
chalund <114383740+chalund@users.noreply.github.com>
2023-03-20 17:51:38 +0100

    new updates

--- js/contact.js
+++ js/contact.js
@@ -6,12 +6,68 @@ const subjectError = document.querySelector("#subjectError");
 const email = document.querySelector("#email");
 const emailError = document.querySelector("#emailError");
 const message = document.querySelector("#formMessage");
-const messageError = document.querySelector("formMessage");
+const messageError = document.querySelector("#messageError");
 const successMessage = document.querySelector("#successMessage");
 
 
-function validateForm() {
-    console.log("hello");
+function validateForm(event) {
+    event.preventDefault();
+
+    let hasErrors = false;
+
+    if (checkLength(fullName.value)=== true) {
+        fullNameError.style.display = "none";
+    } else {
+        fullNameError.style.display = "block";
+        hasErrors = true;
+    }
+
+    if (validateEmail(email.value) === true) {
+        emailError.style.display = "none";
+    } else {
+        emailError.style.display = "block";
+        hasErrors = true;
+    }
+
+    if (checkLength(subject.value)=== true) {
+        subjectError.style.display ="none";
+    } else {
+        subjectError.style.display = "block";
+        hasErrors = true;
+    }
+
+    if (checkLength(message.value) === true) {
+        messageError.style.display = "none";
+    } else {
+        messageError.style.display = "block";
+        hasErrors = true;
+    }
+
+    if (!hasErrors === true) {
+        successMessage.style.display = "block";
+    } else {
+        successMessage.style.display = "none";
+    }
+
+
 }
 
 form.addEventListener("submit", validateForm);
+
+
+
+function checkLength(value) {
+
+    if(value.trim().length) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+function validateEmail(email) {
+    const regEx = /\S+@\S+\.\S+/;
+    const patternMatches = regEx.test(email);
+    return patternMatches;
+}
