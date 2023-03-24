3d7bc6a2c2966ca62df9799e3c361c9389e8dac9
chalund <114383740+chalund@users.noreply.github.com>
2023-03-24 11:41:02 +0100

    updates

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const btnSizeList = document.querySelectorAll(".btn-size");
+
+btnSizeList.forEach(btnSize => {
+    btnSize.addEventListener("click", () => {
+    document.querySelector(".btn-clicked")?.classList.remove("btn-clicked")
+    btnSize.classList.add("btn-clicked");
+});
+})
